8275e38e4c37d3e4f22cb7d9577021e25dbea1ae
her0 <gupo941020@naver.com>
2024-02-02 14:51:11 +0900

    [ADD] commonDAO.class 추가

---
+++ src/models/commonDAO.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import lombok.Getter;
+
+public class commonDAO {
+	private static final String url = "jdbc:mysql://172.20.21.99:3306/bbs";
+	private static final String user = "hero";
+	private static final String password = "1234321";
+	@Getter
+	private PreparedStatement pstmt;
+	@Getter
+	private Connection conn;
+	@Getter
+	private ResultSet rs;
+
+	public void connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			if (conn != null)
+				conn.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (rs != null)
+				rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
